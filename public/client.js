3ee2bbf5af49b01a3dc8b559f16f0e0efb5de2b7
Matthew Stanciu <mattbstanciu@gmail.com>
2019-08-07 18:40:08 -0400

    Optimize winner function

--- public/client.js
+++ public/client.js
@@ -2,17 +2,18 @@ const socket = io()
 
 //localStorage.tap_count = 0
 
-if (typeof (Storage) !== 'undefined') {
+if (typeof Storage !== 'undefined') {
   if (!localStorage.tap_count) localStorage.tap_count = 0
 }
 
-socket.on('updateCounts', (globalCount) => {
+socket.on('updateCounts', globalCount => {
   updateCounts(globalCount)
 })
 
-socket.on('winner', (prize) => {
+socket.on('winner', prize => {
+  document.getElementById('winnerModal').style.display = 'block'
+
   var prizeText = document.getElementById('prize')
-  document.getElementById('winnerModal').style.display = "block"
   prizeText.innerHTML = ` ${prize}`
 
   makeItRain()
@@ -22,15 +23,20 @@ function incrementCount() {
   socket.emit('incrementGlobalCount')
 
   localStorage.tap_count++
-  document.getElementById('local_count').innerHTML = `Personal tap count: ${localStorage.tap_count}`
+  document.getElementById('local_count').innerHTML = `Personal tap count: ${
+    localStorage.tap_count
+  }`
 }
 
 function updateCounts(globalCount) {
-  document.getElementById('local_count').innerHTML = `Personal tap count: ${localStorage.tap_count}`
-  document.getElementById('global_count').innerHTML = `Schoolwide tap count: <span>${globalCount}</span>`
+  document.getElementById('local_count').innerHTML = `Personal tap count: ${
+    localStorage.tap_count
+  }`
+  document.getElementById(
+    'global_count'
+  ).innerHTML = `Schoolwide tap count: <span>${globalCount}</span>`
 }
 
 function checkEmpty() {
-  if (document.getElementById('email').value === '')
-    return false
+  if (document.getElementById('email').value === '') return false
 }
