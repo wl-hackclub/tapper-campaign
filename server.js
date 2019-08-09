3ec8f3bb64d4bb9542324c6d7a1095e21cf060ae
Matthew Stanciu <mattbstanciu@gmail.com>
2019-08-09 08:16:23 -0400

    Dynamically set port

--- server.js
+++ server.js
@@ -86,4 +86,6 @@ io.on('connection', socket => {
   })
 })
 
-http.listen(3000)
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+})
