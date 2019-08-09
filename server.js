851c255d61ea8a13865b0c9dabd17b2ff346e5ea
Matthew Stanciu <mattbstanciu@gmail.com>
2019-08-09 08:18:25 -0400

    shut up

--- server.js
+++ server.js
@@ -86,6 +86,4 @@ io.on('connection', socket => {
   })
 })
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-})
+http.listen(process.env.PORT || 3000)
