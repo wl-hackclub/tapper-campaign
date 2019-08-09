891fa2d8706fa37e499faaeefd774f49f60d2bf4
Matthew Stanciu <mattbstanciu@gmail.com>
2019-08-09 08:29:21 -0400

    back to normal

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ io.on('connection', socket => {
     // console.log(store.get('globalCount'))
 
     switch (store.get('globalCount')) {
-      case 1452:
+      case 1000:
         socket.emit('winner', 'a 10,400mAh portable charger')
         break
       case 4000:
