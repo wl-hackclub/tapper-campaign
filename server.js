34efa3ffda787b3fe713506944066836a9499f8f
Matthew Stanciu <mattbstanciu@gmail.com>
2019-08-09 19:25:49 -0400

    Take it down for the weekend

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/down.html')
 })
 
 app.post('/submission', (req, res) => {
